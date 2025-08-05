256f1a7f03444b211a74d50bcc87552f8205ebf5
AllenTW - Allenpixel服主個人帳號 <allenmc.su+github@gmail.com>
2025-08-05 22:19:03 +0800

    骷髏

---
+++ Mineplex Arcade + Hub + Bungee (BSMC)/Mineplex Bungeecord V2.0/start.bat
@@ -0,0 +1,4 @@
+@echo off
+title mineplex
+java -Xmx256M -Xms256M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -Dfile.encoding=UTF-8 -jar BungeeCord.jar
+exit
